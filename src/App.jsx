1be74ea3ab20efa138d2dfb0268a81de2027fa2b
Subpawad <subpawad648115@gmail.com>
2024-03-16 14:59:27 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ImageProfile } from './components/ImageProfile'
+import { Title } from './components/Title'
 import './App.css'
-import React from 'react';
-import { ImageProfile } from './component/ImageProfile';
-import { Title } from './component/Title';
-
 
 function App() {
-  const {hide , setHide} = useState(false)
+  const [hide, setHide] = useState(false)
 
   return (
     <main id='main'>
       <div className='leftPort'>
-        <ImageProfile image="https://i.pinimg.com/564x/10/c3/7e/10c37e8cc44f1049ad574f4562477e44.jpg" />
+        <ImageProfile image="/src/assets/react.svg" />
 
-        <Title title="Subpawad Sardsanga (Job)">
+        <Title title="Jakkrit Jaroentham (Note)">
           <h3>Student</h3>
         </Title>
 
-        <div className='name'>
-          <h2>Subpawad Sardsanga (Job)</h2>
-        </div>
-        
+        <Title title='name'>
+          <h3>Jakkrit Jaroentham (Note)</h3>
+        </Title>
         <Title title='contact'>
-          <p>
+          <p style={{ display: hide ? "none" : "block" }}>
             Tel :+66644475341
           </p>
+          <button onClick={() => setHide(!hide)}>Hide</button>
           <p>Email : s64122202062@ssru.ac.th</p>
 
+          <Title title='Education'>
+            <p>Suan Sunandha Rajabhat University</p>
+          </Title>
         </Title>
-        <div className='education'>
 
-        </div>
       </div>
 
       <div className="rightPort">
-        <div className="profile">
-
-        </div>
-        <div className="workEx">
-
-        </div>
-        <div className="skills">
-
-        </div>
+        <Title title="Profile">
+          <p>Lorem ipsum dolor 
+            sit amet consectetur adipisicing 
+            elit. Ratione earum in suscipit accusantium id ut facilis quas tempore, sed necessitatibus consectetur hic velit. Provident fuga pariatur nisi quibusdam alias laboriosam.</p>
+        </Title>
+        <Title title="Work Experience">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas assumenda consectetur minima fugit et qui eveniet deleniti aliquid libero maxime, blanditiis, quasi harum nisi similique voluptatem. Similique dolores porro quod.</p>
+        </Title>
+        <Title title="Skills">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, minima distinctio. Magni molestias dolore qui quos voluptatibus explicabo cum repellat nihil mollitia doloribus harum totam ad, molestiae eveniet voluptatum obcaecati.</p>
+        </Title>
       </div>
 
 
@@ -53,4 +52,3 @@ function App() {
 }
 
 export default App
-
--- src/component/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Title = (props) => {
-    return
-    <div className="Titleprot">
-        <h2>{props.Title}</h2>
-        {props.children}
-    </div>
-}
